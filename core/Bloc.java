08c27f5dcbb692f42bdbbbe9b0cf85ebcf975912
kurowasan <philippebrouillard@gmail.com>
2013-07-19 15:25:48 -0400

    Fixed_List2

--- core/Bloc.java
+++ core/Bloc.java
@@ -129,13 +129,56 @@ public class Bloc {
 
 		markSPG_SPD_SM_DM(myStimulus, sPG_SPD_SM_DM);	
 		
+		//hardcodé - en test
+		boolean fixedList = true;
 		
-		boolean fixedList = false;
 		
 		
 		
-		if(fixedList == true){
+		if(version == 5){
+			String myList_0back = "1,7,3,4,2,3,5,3,3,6,3,7,3,9,4,2,3,3,6,3,5,8,3,5,8,3,3,2,4,3,6,1,9,3,3,8,6,3,8,7,3,4,3,1,3,5,3,3,8,1,3,6,4,3,2,3,7,3,9,3,8,7,3,1,2,9,3,5,3,4,3,6,3,7,9,8,2,4,5,3,1,3,3,9,7,6,4,3,3,2,3,1,5,3,8,3,9,3,6,5,3";
+			String myList_1back = "2,6,5,5,3,2,7,6,6,9,9,1,8,8,8,9,2,2,2,7,8,8,5,8,8,5,5,1,1,4,9,9,6,3,3,5,5,3,4,9,8,8,9,9,1,4,8,5,5,5,7,7,7,8,2,2,9,9,1,1,4,4,3,3,7,7,2,1,1,4,4,5,9,3,3,3,2,2,5,5,3,3,7,2,4,4,3,6,6,2,7,6,6,7,7,6,6,6,8,1,6";
+			String myList_2back = "3,8,3,8,7,2,7,5,7,9,7,6,7,2,8,4,8,2,7,2,9,2,8,3,9,3,9,6,9,2,9,6,9,7,5,1,5,1,7,1,6,5,9,5,7,5,2,5,2,3,5,2,4,2,8,3,4,3,2,4,8,4,7,4,1,6,1,6,7,3,7,3,4,1,4,8,5,8,9,8,4,8,5,1,5,6,1,6,8,6,2,9,6,8,6,3,6,1,3,4,1";
+			String myList_3back = "3,7,8,3,7,4,2,6,2,8,9,5,8,2,5,7,2,3,7,4,9,6,4,1,5,4,6,5,6,2,4,6,2,7,6,7,7,4,7,2,3,9,2,8,1,2,1,5,6,1,3,6,7,3,6,2,8,5,7,8,5,8,4,3,8,9,3,1,2,3,1,9,6,1,9,4,1,9,2,6,9,4,6,5,4,9,1,4,5,7,4,8,7,8,5,3,9,5,1,9,8";
+			String myList = "";
 			
+			if(nBack == 0){
+				myList = myList_0back;
+			}else if(nBack == 1){
+				myList = myList_0back;
+			}else if(nBack == 2){
+				myList = myList_0back;
+			}else{
+				myList = myList_0back;
+			}
+			
+			
+			
+			// 1 7 3 4
+			String[] arrFixedList = myList.split(","); 
+			
+			stimulusComplet = new Stimulus[arrFixedList.length];
+			
+			
+			 for (;compteur < stimulusComplet.length; compteur ++ )
+			 {
+				 stimulusComplet[compteur] = CopieSansPointeur(myStimulus[ (Integer.parseInt(arrFixedList[compteur]) - 1) ]);
+				 
+				 if(nBack == 0){
+					 if(Integer.parseInt(arrFixedList[compteur]) - 1 == 2)
+						 stimulusComplet[compteur].setMatch("isMatching") ;
+					 else
+						 stimulusComplet[compteur].setMatch("notMatching") ;
+				 }else{
+					 if(Math.abs(nBack) <= compteur){
+						 if(stimulusComplet[compteur].getName() == stimulusComplet[compteur + nBack].getName())
+							 stimulusComplet[compteur].setMatch("isMatching") ;
+						 else
+							 stimulusComplet[compteur].setMatch("notMatching") ;
+					 }
+				 }
+		     }
+		     
 			
 		}else if (!typeNback.equals ("matching"))
 		{
@@ -156,10 +199,9 @@ public class Bloc {
 			for (;compteur + nBack < 0; compteur ++)
 			{
 				if (compteur<myStimulus.length)
-					stimulusComplet[compteur] = CopieSansPointeur(myStimulus[compteur]);
+					stimulusComplet[compteur] = CopieSansPointeur(myStimulus[compteur]); // Compteur ??? Erreur va faire un suite 1 2 3
 				else 
 					stimulusComplet[compteur] = CopieSansPointeur(myStimulus[0]);
-				System.out.println("Entre dans le pré-nBack");
 			}
 			
 
@@ -170,9 +212,9 @@ public class Bloc {
 		    	 if (rgen.nextInt(100) <40)
 		    	 {
 		    		 //Exception pour la tâche de Phillips où le stim est hardcodé 3
-		    		 if(version == 5 && nBack == 0)
-		    			 stimulusComplet[compteur] = CopieSansPointeur(myStimulus[2]); 
-		    		 else
+		    		 //if(version == 5 && nBack == 0)
+		    			 //stimulusComplet[compteur] = CopieSansPointeur(myStimulus[2]); 
+		    		 //else
 		    			stimulusComplet[compteur] = CopieSansPointeur(stimulusComplet[compteur+nBack]);
 		    		 stimulusComplet[compteur].setMatch("isMatching") ;
 		    	 }
@@ -182,7 +224,9 @@ public class Bloc {
 		    		 //***//Phil
 		    		 boolean first = true;
 			    	 String lastStimulusName = "";
-			    	 if(version == 5 && nBack == 0){
+			    	 
+			    	 
+			    	 /*if(version == 5 && nBack == 0){
 		    			 if(first == true)
 		    				 first = false;
 		    			 else
@@ -193,22 +237,22 @@ public class Bloc {
 			    		 while (lastStimulusName == myStimulus[hasard].getName() || hasard == 2);
 	    			 
 		    			 first = false;
-		    		 }else{
+		    		 }else{*/
 			    		 do {
 			    			 rgen = new Random();
 			    			 hasard = rgen.nextInt(myStimulus.length);
 			    		 }
 			    		 while (stimulusComplet[compteur+nBack].getName() == myStimulus[hasard].getName());
-		    	 	 }
+		    	 	 //}
 		    		 
-		    		 if(version == 5 && nBack == 0){
+		    		/* if(version == 5 && nBack == 0){
 		    			 stimulusComplet[compteur] = CopieSansPointeur(myStimulus[hasard]);
 				 		 stimulusComplet[compteur].setMatch("notMatching") ;
 		    			 stimulusComplet[compteur].setKey("f");
-		    		 }else{
+		    		 }else{*/
 				 		 stimulusComplet[compteur] = CopieSansPointeur(myStimulus[hasard]);
 			 			 stimulusComplet[compteur].setMatch("notMatching") ;
-		    		 }
+		    		 //}
 		    		 //***//
 		    	 }
 		    }
